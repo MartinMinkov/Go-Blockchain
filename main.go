c5191fb0f4a38cefcd19c78843da67b9c4e26924
Martin Minkov <minkovlmartin@gmail.com>
2021-03-23 22:04:02 -0700

    Added blocks creation endpoint

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -13,14 +16,38 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/blocks", returnAllBlocks)
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/", homePage)
+	r.HandleFunc("/api/blocks", returnAllBlocks).Methods("GET")
+	r.HandleFunc("/api/mine", createBlock).Methods("POST")
+
 	fmt.Println("Listening on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func returnAllBlocks(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllBlocks")
+	json.NewEncoder(w).Encode(Mainchain)
+}
+
+func createBlock(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	t := struct {
+		Data string
+	}{}
+
+	err := json.Unmarshal(body, &t)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if t.Data == "" {
+		fmt.Println("No data supplied for block")
+		return
+	}
+
+	Mainchain.addBlock(Data(t.Data))
 	json.NewEncoder(w).Encode(Mainchain)
 }
 
